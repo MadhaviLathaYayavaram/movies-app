89380c075bf3ea0cc360fcbe258b3840dc924fb1
Madhavi Latha Yayavaram <lathayvm@gmail.com>
2022-01-03 19:36:59 +0530

    movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,46 +1,45 @@
 import { useState } from "react";
 import {Counter} from "./counter";
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 
-export function Movie({mv}) {
-  const movies = [{
-    moviename:mv.moviename, 
-    poster:mv.poster,
-    rating:mv.rating, 
-    summary:mv.summary,}];
+export function Movie({moviename,rating, summary,poster}) {
+  const movie = [{
+    moviename:moviename, 
+    poster:poster,
+    rating:rating, 
+    summary:summary,}];
 
-  const styles = {color: movies.rating<8 ? "crimson" :"teal" }; 
+  const styles = {color: movie.rating<8 ? "crimson" :"teal" }; 
   const [show,setShow] = useState(true);
    const summaryStyles = {display : show ? "block" : "none"};
 
   return (
         <div>
-         { movies.forEach (   
-
-        <Card className ="movie-container">
-        <img src={movies.poster}
-          alt={movies.moviename}
+  
+            <Card className ="movie-container">
+        <img src={movie.poster}
+          alt={movie.moviename}
           className="movie-poster"></img>
         <div className="movie-specs">
-        <p className="movie-name">{movies.moviename}
+        <p className="movie-name">{movie.moviename}
         <IconButton aria-label="hide" color='primary' onClick = {  () => setShow(!show)}>
 
            {show ? <ExpandLessIcon /> :<ExpandMoreIcon />}  
                    
         </IconButton></p>
-        <p className="movie-rating" style={styles}>*{movies.rating}</p>
+        <p className="movie-rating" style={styles}>*{movie.rating}</p>
         </div>
           
               {/*
         <Button variant="text" onClick = {  () => setShow(!show)}>
       {show ? "Hide" :"Show"} Description </Button>  */}
   
-        <p className="movie-summary" style={summaryStyles}>{movies.summary}</p>
+        <p className="movie-summary" style={summaryStyles}>{movie.summary}</p>
 
         <CardActions>
         <Counter />
@@ -48,7 +47,6 @@ export function Movie({mv}) {
 
         </Card>
 
-        )  }
         </div>
         );
         
